8b7be03921d23f8498d7383b69ef0b99bc607cb9
mrleeno <mrleeno@gmail.com>
2022-08-26 20:08:27 +0900

    2022.08.26

---
+++ SoloLearn#4.py
@@ -0,0 +1,21 @@
+#Generators
+
+def isPrime(x):
+    if x < 2:
+        return False
+    elif x == 2:
+        return True
+    for n in range(2, x):
+        if x % n == 0:
+            return False
+    return True
+
+def primeGenerator(a, b):
+    for n in range(a, b):
+        if isPrime(n) == True:
+            yield n
+
+f = int(input())
+t = int(input())
+
+print(list(primeGenerator(f, t)))
