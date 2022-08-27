d1245076f2ff13be5974bf2092442b1f9540cbb6
mrleeno <mrleeno@gmail.com>
2022-08-27 20:23:44 +0900

    List Comprehension

--- SoloLearn#3.py
+++ SoloLearn#3.py
@@ -1,10 +1,10 @@
 #List Comprehension
 
-#evens = [i**2 for i in range(10) if i**2 % 2 == 0]
-#print(evens)
+evens = [i**2 for i in range(10) if i**2 % 2 == 0]
+print(evens)
 
-#nums = [i*2 for i in range(10)]
-#print(nums)
+nums = [i*2 for i in range(10)]
+print(nums)
 
 word = input()
 vowels = ["a","e","i","o","u"]
